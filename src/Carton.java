15d2249d67449442d2f1cc866a93c67df15bd9dc
chrisgarland <1chris.garland1@gmail.com>
2015-09-15 20:28:29 +0800

    With accessors/mutators, exception handling & comments

--- src/Carton.java
+++ src/Carton.java
@@ -10,15 +10,81 @@ public class Carton
 
     public Carton( int consignmentNote, String productType, String wholesalerName, String warrantyDate )
     {
-        validateParameters( consignmentNote, productType, wholesalerName, warrantyDate );
+        try
+        {
+            validateParameters( consignmentNote, productType, wholesalerName, warrantyDate );
+        }
+        catch( IllegalArgumentException e )
+        {
+            System.out.println( e.getMessage() );
+        }
 
+        setConsignmentNote( consignmentNote );
+        setProductType( productType );
+        setWholesalerName( wholesalerName );
+        setWarrantyDate( warrantyDate );
+    }
+
+
+//Accessors:
+
+    public int getConsignmentNote()
+    {
+        return consignmentNote;
+    }
+
+    public String getProductType()
+    {
+        return productType;
+    }
+
+    public String getWholesalerName()
+    {
+        return wholesalerName;
+    }
+
+    public String getWarrantyDate()
+    {
+        return warrantyDate;
+    }
+
+
+//Accessors: Exceptions are handled in constructor
+
+    public void setConsignmentNote( int consignmentNote )
+    {
         this.consignmentNote = consignmentNote;
+    }
+
+    public void setProductType( String productType )
+    {
         this.productType = productType;
+    }
+
+    public void setWholesalerName( String wholesalerName )
+    {
         this.wholesalerName = wholesalerName;
+    }
+
+    public void setWarrantyDate( String warrantyDate )
+    {
         this.warrantyDate = warrantyDate;
     }
 
-    private void validateParameters( int consignmentNote, String productType, String wholesalerName, String warrantyDate )
+
+    /**
+     * Purely for exception handling purposes.
+     *
+     * @param consignmentNote - Asserts int > 0 && < 1024.
+     * @param productType - Asserts String not null && not empty.
+     * @param wholesalerName - Asserts String not null && not empty.
+     * @param warrantyDate - Asserts String not null && not empty.
+     * @throws IllegalArgumentException
+     */
+    private void validateParameters( int consignmentNote,
+                                     String productType,
+                                     String wholesalerName,
+                                     String warrantyDate ) throws IllegalArgumentException
     {
         if( consignmentNote < 0 || consignmentNote == 0 )
         {
