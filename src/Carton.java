87fa841555ad6092639647d6a458e082913b7acb
chrisgarland <1chris.garland1@gmail.com>
2015-09-15 18:31:29 +0800

    Needs work

--- src/Carton.java
+++ src/Carton.java
@@ -3,4 +3,45 @@
  */
 public class Carton
 {
+    private int consignmentNote;
+    private String productType;
+    private String wholesalerName;
+    private String warrantyDate;
+
+    public Carton( int consignmentNote, String productType, String wholesalerName, String warrantyDate )
+    {
+        validateParameters( consignmentNote, productType, wholesalerName, warrantyDate );
+
+        this.consignmentNote = consignmentNote;
+        this.productType = productType;
+        this.wholesalerName = wholesalerName;
+        this.warrantyDate = warrantyDate;
+    }
+
+    private void validateParameters( int consignmentNote, String productType, String wholesalerName, String warrantyDate )
+    {
+        if( consignmentNote < 0 || consignmentNote == 0 )
+        {
+            throw new IllegalArgumentException( "Consignment Note must be greater than 0" );
+        }
+        if( consignmentNote == 1024 || consignmentNote > 1024 )
+        {
+            throw new IllegalArgumentException( "Consignment Note must be less than 1024" );
+        }
+
+        if( productType == null || productType.isEmpty() )
+        {
+            throw new IllegalArgumentException( "No Product Type provided" );
+        }
+
+        if( wholesalerName == null || wholesalerName.isEmpty() )
+        {
+            throw new IllegalArgumentException( "No Wholesaler Name provided" );
+        }
+
+        if( warrantyDate == null || warrantyDate.isEmpty() )
+        {
+            throw new IllegalArgumentException( "No Warranty Date provided" );
+        }
+    }
 }
