9091e76b03a1e41e239a71c007eb2d7302a3dcae
chrisgarland <1chris.garland1@gmail.com>
2015-09-16 11:02:51 +0800

    Not pretty

---
+++ src/Yard.java
@@ -0,0 +1,75 @@
+import javax.naming.OperationNotSupportedException;
+
+/**
+ * Created by chrisgarland on 16/09/15.
+ */
+public class Yard extends StockRoom
+{
+    @Override
+    public void insert( Object inValue ) throws IllegalArgumentException, IllegalStateException
+    {
+        boolean insertSuccessful = false;
+        int index = 0;
+
+        if( inValue == null )
+        {
+            throw new IllegalArgumentException( "Null value provided" );
+        }
+        if( isFull() )
+        {
+            throw new IllegalStateException( "Yard is full. Cannot insert value" );
+        }
+
+        while( !( insertSuccessful ) )
+        {
+            if( storageBank[index] == null )
+            {
+                storageBank[index] = inValue;
+                numElements++;
+                insertSuccessful = true;
+            }
+
+            index++;
+        }
+
+        if( !( insertSuccessful ) )
+        {
+            throw new IllegalStateException( "Value was not inserted into Yard" );
+        }
+    }
+
+    @Override
+    public Object remove()
+    {
+        try
+        {
+            throw new OperationNotSupportedException( "Operation not supported. Must provide index" );
+        }
+        catch( OperationNotSupportedException e )
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Object peek()
+    {
+        return null;
+    }
+
+    public Object remove( int index )
+    {
+        Object value;
+
+        if( isEmpty() )
+        {
+            throw new IllegalStateException( "Yard is empty. Cannot remove element" );
+        }
+
+        value = storageBank[index];
+        numElements--;
+
+        return value;
+    }
+}
